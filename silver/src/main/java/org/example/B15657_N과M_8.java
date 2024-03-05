d40d71122a2435749b50772555d829fdcc7722be
fkausajrrl <118492599+fkausajrrl@users.noreply.github.com>
2024-03-05 10:54:32 +0900

    create B15655,B15656,B15657.java
    N과M 시리즈 끝

---
+++ silver/src/main/java/org/example/B15657_N과M_8.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B15657_N과M_8 {
+    static int[] perm; //N까지의 숫자 저장된 배열
+    static int[] result; //결과 저장할 배열
+    static StringBuilder sb = new StringBuilder();
+
+    static void permutation(int start,int ind, int M) {
+        if (start > M) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = ind; i <= perm.length - 1; i++) {
+            result[start] = perm[i];
+            permutation(start + 1, i, M); //재귀 실행
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        perm = new int[N + 1];
+        perm[0] = 0; //내림차순 정려하려고 넣기
+
+        for (int i = 1; i <= N; i++) {
+            perm[i] = sc.nextInt(); //N까지 숫자 기입
+        }
+        Arrays.sort(perm);
+        result = new int[N + 1]; //결과 저장할 배열
+
+        permutation(1, 1, M);
+        System.out.print(sb.toString());
+    }
+}
